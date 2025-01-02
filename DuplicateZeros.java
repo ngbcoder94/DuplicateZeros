a95b4e33c45c2cd663462b79110910653bbde586
ngbcoder94 <ngbcoder94@gmail.com>
2025-01-01 23:10:10 -0500

    DuplicateZeros class created

---
+++ DuplicateZeros.java
@@ -0,0 +1,17 @@
+/*
+ * This is my solution to the Leetcode problem of Duplicate Zeros. 
+ *
+ * Author: Nick Burkett
+ * Date: 01/01/25
+ *
+ * Status - WIP
+ */
+public class DuplicateZeros{
+        
+        public static void main(String[] args){
+                
+                System.out.println("Hello World");
+        }
+
+}
+
