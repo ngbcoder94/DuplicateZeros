13fa110e1ac1a1775e08ed88958ad8dc93d90a22
ngbcoder94 <ngbcoder94@gmail.com>
2025-01-02 21:16:23 -0500

    Helper function created for insert()

--- DuplicateZeros.java
+++ DuplicateZeros.java
@@ -8,10 +8,18 @@
  */
 public class DuplicateZeros{
         
-        public static void main(String[] args){
-                
-                System.out.println("Hello World");
+        /*
+         * Here is my insertZero function that will insert a zero into the array. 
+         *
+         * @param int[] nums - Array passed in to be modified.
+         * @param int index - Where we need to stop in the loop.
+         * @return void - Don't return anything.
+         */
+        public void insertZero(int[] nums, int index){
+
         }
+
+        public static void main(String[] args){}
 
 }
 
